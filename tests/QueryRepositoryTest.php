c0f551536c6fddbe9d77a31dd4f1729b63e310b0
Rougin Gutib <rougingutib@gmail.com>
2019-09-03 21:16:30 +0800

    Update QueryRepositoryTest

--- tests/QueryRepositoryTest.php
+++ tests/QueryRepositoryTest.php
@@ -142,6 +142,8 @@ class QueryRepositoryTest extends \PHPUnit_Framework_TestCase
 
         $result = $this->repo->set($mutator)->first();
 
+        echo json_encode($result);
+
         $this->assertEquals($expected, $result);
     }
 
